741517e0c2dca2421037040ec2af9f0bde99767b
Dossantosjoel7 <88427827+Dossantosjoel7@users.noreply.github.com>
2025-05-27 21:18:00 +0100

    Update JogadorActual.jsx

--- src/components/JogadorActual/JogadorActual.jsx
+++ src/components/JogadorActual/JogadorActual.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "../JogadorActual/JogadorActual.css"
 
 
-const JogadorActual = ({jogador,color,onRestart}) => {
+const JogadorActual = ({jogador,color,onRestart,tempoRestante}) => {
     color = color === "cor1"? "var(--ColorJogador1)": "var(--ColorJogador2) ";
     return (
         <div className="status">
             <div className="status-color">
                 <span style={{backgroundColor:color, borderColor:color}}></span>
                 <p>É a sua vez {jogador}</p>
+                <strong>  ⏱️ {tempoRestante}s</strong>
             </div>
             <button type="button" className="btn btn-danger btn-sm" onClick={onRestart} >Reiniciar</button>
         </div>
